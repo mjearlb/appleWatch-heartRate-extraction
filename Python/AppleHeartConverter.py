fe1895d40a510101c42270fb851fc0849b82e5c0
Milo Bauman <mjb01161@uga.edu>
2024-05-21 12:13:35 -0400

    Now returns HRV data

--- Python/AppleHeartConverter.py
+++ Python/AppleHeartConverter.py
@@ -14,9 +14,19 @@ def ExtractHeartRateData(filePath):
     heartRateVariabilityRecords = rootElm.findall(".//Record[@type='HKQuantityTypeIdentifierHeartRateVariabilitySDNN']")
 
     # Extract the 'value' attribute from each record
-    heartRates = [record.get('value') for record in heartRateRecords]
+    heartRates = [(record.get('startDate'), record.get('value')) for record in heartRateRecords]
 
-    return heartRates
+    # Extract the 'value' and 'startDate' attributes from each HRV record
+    heartRateVariabilities = [(record.get('value'), record.get('startDate')) for record in heartRateVariabilityRecords]
+
+    # Combine heart rate and HRV data
+    combinedData = heartRates + heartRateVariabilities
+
+    # Sort the combined data by timestamp
+    combinedData.sort(key=lambda x: x[1])
+
+    return combinedData
+
 
 # Use the function
 heartRates = ExtractHeartRateData('../../../Desktop/apple_health_export/export.xml')
